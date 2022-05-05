962492bf65e5499b9f57ec03158767fb4f69e4d8
Anton Palermo <ANPBusinessmail@gmail.com>
2022-05-05 08:24:18 +0800

    initialize prisma

---
+++ prisma/users.ts
@@ -0,0 +1,35 @@
+export interface User {
+  name: string
+  email: string
+}
+
+export const users: User[] = [
+  {
+    name: 'Anton Palermo',
+    email: 'AntonPalermo@gmail.com'
+  },
+  {
+    name: 'Partick Stuwart',
+    email: 'PartickStuwart@gmail.com'
+  },
+  {
+    name: 'Jake Summers',
+    email: 'JakeSummers@gmail.com'
+  },
+  {
+    name: 'Kate Calum',
+    email: 'KateCalum@gmail.com'
+  },
+  {
+    name: 'Micky Barns',
+    email: 'MickyBarns@gmail.com'
+  },
+  {
+    name: 'Jack Samson',
+    email: 'JackSamson@gmail.com'
+  },
+  {
+    name: 'Jane Doe',
+    email: 'JaneDoe@gmail.com'
+  }
+]
