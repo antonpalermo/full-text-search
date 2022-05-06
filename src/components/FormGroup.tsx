455b3d71740c1a39321bf3cc3b30edb4c118c00a
Anton Palermo <ANPBusinessmail@gmail.com>
2022-05-06 23:02:37 +0800

    popper

--- src/components/FormGroup.tsx
+++ src/components/FormGroup.tsx
@@ -1,5 +1,22 @@
 import React, { useState } from 'react'
-import { FormControl, FormLabel, Input } from '@chakra-ui/react'
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  useColorModeValue,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+  Text,
+  Stack
+} from '@chakra-ui/react'
 import { Form, Formik } from 'formik'
 
 import { debounce } from 'lodash'
@@ -9,12 +26,22 @@ export function FormGroup() {
     query: ''
   }
 
+  const [open, setOpen] = useState(false)
   const [data, setData] = useState([])
 
   async function fuzzySearch(keyword: string) {
     const req = await fetch(`/api/search?keyword=${keyword}`)
     const result = await req.json()
-    setData(result)
+    if (
+      typeof result === 'object' &&
+      Array.isArray(result) &&
+      result.length >= 1
+    ) {
+      setOpen(true)
+      setData(result)
+    } else {
+      setOpen(false)
+    }
   }
 
   const handleQuery = debounce(value => {
@@ -22,35 +49,41 @@ export function FormGroup() {
   }, 500)
 
   return (
-    <>
-      <Formik initialValues={initialData} onSubmit={() => {}}>
-        {({ values, handleChange }) => (
-          <Form>
-            <FormControl>
-              <FormLabel>Search</FormLabel>
-              <Input
-                name="query"
-                value={values.query}
-                onChange={e => {
-                  handleChange(e)
-                  handleQuery(e.target.value)
-                }}
-                autoComplete="off"
-              />
-            </FormControl>
-          </Form>
-        )}
-      </Formik>
-      {data && (
-        <>
-          {data?.map(user => (
-            <>
-              <h1 key={user.email}>{user.email}</h1>
-              <p>{user.name}</p>
-            </>
-          ))}
-        </>
-      )}
-    </>
+    <Box position={'relative'}>
+      <Popover isOpen={open} autoFocus={false} placement={'bottom'}>
+        <Formik initialValues={initialData} onSubmit={() => {}}>
+          {({ values, handleChange }) => (
+            <Form>
+              <FormControl>
+                <FormLabel>Search</FormLabel>
+                <Input
+                  name="query"
+                  value={values.query}
+                  onChange={e => {
+                    handleChange(e)
+                    handleQuery(e.target.value)
+                  }}
+                  autoComplete="off"
+                />
+              </FormControl>
+            </Form>
+          )}
+        </Formik>
+        <Box position="relative" mt="2">
+          <PopoverContent>
+            <PopoverBody>
+              <Text>Search Result</Text>
+              {data && (
+                <>
+                  {data.map(user => (
+                    <h1 key={user.id}>{user.email}</h1>
+                  ))}
+                </>
+              )}
+            </PopoverBody>
+          </PopoverContent>
+        </Box>
+      </Popover>
+    </Box>
   )
 }
